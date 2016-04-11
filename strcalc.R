8de6fb7a3c4bed264d5a204e9782a9d8f51a8262
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-04-11 22:22:04 +1000

    Improve function documentation with args and returns

--- strcalc.R
+++ strcalc.R
@@ -1,6 +1,13 @@
 compute <- function(input_string) {
   # Perform simple arithmetic encoded in input string
   # e.g. '1 + 2' -> 3, or '1 - 2' -> -1
+  #
+  # Args:
+  #   input_string: the string containing the operation
+  #                 to be performed.
+  #
+  # Returns:
+  #   The numeric result of the arithmetic operation.
   values <- unlist(strsplit(input_string, ' '))
   num0 <- as.integer(values[1])
   operator <- values[2]
